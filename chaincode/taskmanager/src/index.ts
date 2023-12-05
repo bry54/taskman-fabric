ccc44ce77fc2e365425f8f3ede135dbfa5f86931
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2023-12-05 17:42:49 +0200

    Updated chaincode with docker file and pipe file to export all resources.
    Signed-off-by: Brian Paidamoyo Sithole <brianacyth@icloud.com>

---
+++ chaincode/taskmanager/src/index.ts
@@ -0,0 +1,7 @@
+import { TaskManagerContract } from './taskManager';
+import { Task } from "./task";
+
+export { TaskManagerContract } from './taskManager';
+export { Task } from './task';
+
+export const contracts: any[] = [ TaskManagerContract ];
